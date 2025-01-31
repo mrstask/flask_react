87b57e77224a6a711f31b0ff08ad48dee0eeee3a
Stanislav_Lazarenko <stanislav_lazarenko@epam.com>
2025-01-31 15:46:58 +0200

    Simplify app.py execution parameters
    Removed explicit host and port settings in `app.run`, relying on default Flask configuration for simplicity. This ensures cleaner and more maintainable code.

--- app.py
+++ app.py
@@ -37,5 +37,6 @@ def streaming(ws):
             print(f"Error: {e}")
             break
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
+
